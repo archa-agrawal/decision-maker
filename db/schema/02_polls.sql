0c73eb489e5291c6a9da864431020c96d332d1cc
Archana Agrawal <archana.agrawal3@outlook.com>
2022-07-29 17:27:37 -0400

    add schema and seed files

--- db/schema/02_polls.sql
+++ db/schema/02_polls.sql
@@ -5,8 +5,8 @@ CREATE TABLE polls (
   id SERIAL PRIMARY KEY,
   creator_id INTEGER REFERENCES users(id),
   title VARCHAR(255) NOT NULL,
-  choices TEXT[][] NOT NULL
-  name_required BOOLEAN,
+  choices TEXT[][] NOT NULL,
+  name_required BOOLEAN
 );
 
 
